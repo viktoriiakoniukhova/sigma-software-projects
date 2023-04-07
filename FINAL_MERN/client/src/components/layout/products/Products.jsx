82349e5243830bec7743b47e7743d58effee159b
viktoriiakoniukhova <viktoriia.koniukhova@gmail.com>
2023-04-07 11:48:40 +0300

    Scroll to top of "Products" section on "hide" button click

--- FINAL_MERN/client/src/components/layout/products/Products.jsx
+++ FINAL_MERN/client/src/components/layout/products/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Products.module.scss";
 import Button from "../../ui/button/Button";
 
@@ -11,8 +11,14 @@ export default function Products({ data }) {
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const ref = useRef();
+  React.useEffect(() => {
+    if (isFold) window.scrollTo(0, ref.current.offsetTop);
+  }, [isFold]);
+
   return (
-    <div className={styles.productsWrapper} id="products">
+    <div className={styles.productsWrapper} id="products" ref={ref}>
       <div className={styles.productsList}>
         {dataLeft}
         {!isFold && dataRight}
